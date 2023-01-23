f07a5c64695f31baacbce4bb7434ca66cac793e0
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2023-01-23 19:25:31 +0300

    Flutter Card & ListTile Widgets

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -14,18 +14,20 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 50.0,
               backgroundImage: AssetImage('images/IMG_1496.heic'),
             ),
             Text(
-              'Jaona Rantoniaina Andriampionona',
+              'Jaona Rantoniaina Andriamampionona',
               style: TextStyle(
                   fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
             ),
             Text(
               'FULLSTACK DEVELOPER',
@@ -36,48 +38,36 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(color: Colors.teal.shade100),
+            ),
+            Card(
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: [
-                  Icon(
+              child: ListTile(
+                  leading: Icon(
                     Icons.phone,
                     color: Colors.teal,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('+261 34 08 474 99',
+                  title: Text('+261 34 08 474 99',
                       style: TextStyle(
                           color: Colors.teal.shade900,
                           fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0)),
-                ],
-              ),
+                          fontSize: 20.0))),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('jaona.rantoniaina@gmail.com',
-                      style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0)),
-                ],
-              ),
-            )
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text('jaona.rantoniaina@gmail.com',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0))))
           ],
         )),
       ),
