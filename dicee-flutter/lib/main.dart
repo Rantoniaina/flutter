fc416ad977b1fcfb83e17c8458fc734f56a3b0eb
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2023-01-23 20:21:49 +0300

    Making the Dice Image Change Reactively

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -18,12 +18,14 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var leftDiceNumber = 3;
+
     return Center(
       child: Row(
         children: [
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
               onPressed: () {
                 print('Left button got pressed.');
               },
