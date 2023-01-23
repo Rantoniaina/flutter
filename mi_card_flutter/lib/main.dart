c36939f34de55a63ae2ab6af8668c34bc050c920
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2023-01-23 18:51:53 +0300

    Incorporating Custom Fonts in Your Flutter App

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -22,8 +22,18 @@ class MyApp extends StatelessWidget {
             Text(
               'Jaona Rantoniaina Andriampionona',
               style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'FULLSTACK DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
                   fontWeight: FontWeight.bold),
             )
           ],
